a47f7405ed8bec34faeecbc8513f42f4e13a8f18
jyllsarta <illstarred.dive@gmail.com>
2018-03-26 23:23:29 +0900

    マップシーン 座標が並んでおり、移動可能な場所が明るくなる　明るいところをクリックした場合そこに移動する

---
+++ Assets/Scripts/Map/Map.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour {
+
+    public GameObject pointsContainer;
+    public List<MapPoint> points;
+    public MapPoint currentPoint;
+    public float distance;
+
+    //Sceneの座標情報を読み込む
+    public void loadGeometry()
+    {
+        points = new List<MapPoint>();
+        MapPoint[] scenePoints = pointsContainer.GetComponentsInChildren<MapPoint>();
+        foreach (MapPoint p in scenePoints)
+        {
+            points.Add(p);
+        }
+    }
+
+    void updateAvailablePoints()
+    {
+        foreach(MapPoint p  in points)
+        {
+            if (p.isDistanceLessThan(currentPoint.pos, distance))
+            {
+                p.setMoveAvailableState(true);
+            }
+            else
+            {
+                p.setMoveAvailableState(false);
+            }
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+        loadGeometry();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        updateAvailablePoints();
+	}
+}
---
+++ Assets/Scripts/Map/MapPoint.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MapPoint : MonoBehaviour {
+
+    public Image image;
+    public RectTransform pos;
+    public bool isMoveAbailable;
+    public Map map;
+
+    public bool isDistanceLessThan(RectTransform target, float distance)
+    {
+        float d = Vector2.Distance(pos.anchoredPosition, target.anchoredPosition);
+        return d < distance;
+    }
+
+    public void setMoveAvailableState(bool isAvailable)
+    {
+        if (isAvailable)
+        {
+            this.isMoveAbailable = true;
+            image.color = new Color(0.8f, 0.8f, 1f, 1f);
+        }
+        else
+        {
+            this.isMoveAbailable = false;
+            image.color = new Color(0.5f, 0.5f, 0.5f, 1f);
+        }
+    }
+
+    public void setCurrentPositionToThis()
+    {
+        map.currentPoint = this;
+    }
+
+    public void onClick()
+    {
+        if (isMoveAbailable)
+        {
+            setCurrentPositionToThis();
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+        setMoveAvailableState(false);
+        map = FindObjectOfType<Map>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
