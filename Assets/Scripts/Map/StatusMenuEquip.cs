ba367ae27abd4b5010ad3c50962517a3babdff98
jyllsarta <illstarred.dive@gmail.com>
2018-04-14 20:59:32 +0900

    ステータス画面上の装備マウスオーバーで装備の詳細を表示

---
+++ Assets/Scripts/Map/StatusMenuEquip.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.EventSystems;
+
+public class StatusMenuEquip : MonoBehaviour, IPointerEnterHandler
+{
+
+    public TextMeshProUGUI text;
+    public MessageArea messageArea;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        string equipName = text.text;
+        if (text.text == "-" || text.text == null)
+        {
+            messageArea.updateText("-");
+            return;
+        }
+        Equip e = EquipStore.getEquipByName(equipName);
+        messageArea.updateText(e.description);
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
