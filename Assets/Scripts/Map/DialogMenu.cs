98fd483056670fb576451090c338a3c09751feeb
jyllsarta <illstarred.dive@gmail.com>
2018-04-22 19:50:57 +0900

    輪郭線の統一

---
+++ Assets/Scripts/Map/DialogMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DialogMenu : UIMenu {
+    public TextMeshProUGUI content;
+    public TextMeshProUGUI title;
+
+    public void setText(string text)
+    {
+        content.text = text;
+    }
+    public void setTitle(string text)
+    {
+        title.text = text;
+    }
+}
