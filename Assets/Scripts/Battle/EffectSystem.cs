4866f96596f45b645746dcdd82f07d8b8972df27
jyllsarta <illstarred.dive@gmail.com>
2018-05-18 08:07:53 +0900

    攻撃系アクションにエフェクトを追加

--- Assets/Scripts/Battle/EffectSystem.cs
+++ Assets/Scripts/Battle/EffectSystem.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class EffectSystem : MonoBehaviour {
 
     public GameObject blueExplosion;
+    public GameObject icyExplosion;
+    public GameObject fireNova;
+    public GameObject thunder;
+    public GameObject swordSlash;
     public GameObject defaultEffect;
 
     //名前
@@ -14,6 +18,16 @@ public class EffectSystem : MonoBehaviour {
         {
             case "瞬突":
                 return blueExplosion;
+            case "刺突":
+                return blueExplosion;
+            case "双撃":
+                return swordSlash;
+            case "凍結":
+                return icyExplosion;
+            case "雷光":
+                return thunder;
+            case "獄炎":
+                return fireNova;
             default:
                 return defaultEffect;
         }
