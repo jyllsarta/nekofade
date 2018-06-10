79f190158341d5b1178739cba6bc7e9bdbd9c976
jyll sarta <illstarred.dive@gmail.com>
2018-06-10 23:50:07 +0900

    クリアランクを表示

--- Assets/Scripts/Clear/ClearSceneConstructor.cs
+++ Assets/Scripts/Clear/ClearSceneConstructor.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class ClearSceneConstructor : MonoBehaviour {
 
@@ -12,13 +13,39 @@ public class ClearSceneConstructor : MonoBehaviour {
 
     public NumeratableText goldReturn;
     public NumeratableText turnCount;
+    public TextMeshProUGUI goldNextRank;
+    public TextMeshProUGUI turnNextRank;
+    public TextMeshProUGUI goldRank;
+    public TextMeshProUGUI turnRank;
 
     public SpriteRenderer darken;
 
     public SirokoStats status;
 
-	// Use this for initialization
-	void Start () {
+    public void updateRanks()
+    {
+        goldRank.text = RankDefinitions.getRankStringFromGoldAmount(status.gold);
+        turnRank.text = RankDefinitions.getRankStringFromClearTurn(status.clock);
+        if (goldRank.text == "S")
+        {
+            goldNextRank.text = "最高評価です！やったね！";
+        }
+        else
+        {
+            goldNextRank.text = string.Format("あと {0} でランクアップ", RankDefinitions.getAmountToNextRankFromGoldAmount(status.gold));
+        }
+        if (turnRank.text == "S")
+        {
+            turnNextRank.text = "最高評価です！やったね！";
+        }
+        else
+        {
+            turnNextRank.text = string.Format("あと {0} でランクアップ", RankDefinitions.getAmountToNextRankFromClearTurn(status.clock));
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
         congratsTimer = 0;
 
         status = FindObjectOfType<SirokoStats>();
@@ -31,7 +58,7 @@ public class ClearSceneConstructor : MonoBehaviour {
 
         goldReturn.numerate(status.gold);
         turnCount.numerate(status.clock);
-
+        updateRanks();
 	}
 	
 	// Update is called once per frame
@@ -49,7 +76,10 @@ public class ClearSceneConstructor : MonoBehaviour {
 
     public void returnToTitle()
     {
-        Destroy(status);
+        foreach (SirokoStats status in FindObjectsOfType<SirokoStats>())
+        {
+            Destroy(status.gameObject);
+        }
         SceneManager.LoadSceneAsync("title");
     }
 }
---
+++ Assets/Scripts/Clear/RankDefinitions.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class RankDefinitions {
+
+    public static string getRankStringFromClearTurn(int clearTurn)
+    {
+        if (clearTurn <= 35)
+        {
+            return "S";
+        }
+        if (clearTurn <= 40)
+        {
+            return "A";
+        }
+        if (clearTurn <= 50)
+        {
+            return "B";
+        }
+        if (clearTurn <= 70)
+        {
+            return "C";
+        }
+        if (clearTurn <= 100)
+        {
+            return "D";
+        }
+        return "E";
+    }
+
+    public static int getAmountToNextRankFromClearTurn(int clearTurn)
+    {
+        if (clearTurn <= 35)
+        {
+            Debug.Log("いやもう最強やんけ");
+            return 0;
+        }
+        if (clearTurn <= 40)
+        {
+            return clearTurn - 35;
+        }
+        if (clearTurn <= 50)
+        {
+            return clearTurn - 40;
+        }
+        if (clearTurn <= 70)
+        {
+            return clearTurn - 50;
+        }
+        if (clearTurn <= 100)
+        {
+            return clearTurn - 70;
+        }
+        return clearTurn - 100;
+    }
+
+    public static string getRankStringFromGoldAmount(int goldAmount)
+    {
+        if (goldAmount >= 1000)
+        {
+            return "S";
+        }
+        if (goldAmount >= 800)
+        {
+            return "A";
+        }
+        if (goldAmount >= 600)
+        {
+            return "B";
+        }
+        if (goldAmount >= 400)
+        {
+            return "C";
+        }
+        if (goldAmount >= 300)
+        {
+            return "D";
+        }
+        return "E";
+    }
+
+    public static int getAmountToNextRankFromGoldAmount(int goldAmount)
+    {
+        if (goldAmount >= 1000)
+        {
+            Debug.Log("いやもう最強やんけ");
+            return 0;
+        }
+        if (goldAmount >= 800)
+        {
+            return 1000 - goldAmount;
+        }
+        if (goldAmount >= 600)
+        {
+            return 800 - goldAmount;
+        }
+        if (goldAmount >= 400)
+        {
+            return 600 - goldAmount;
+        }
+        if (goldAmount >= 300)
+        {
+            return 400 - goldAmount;
+        }
+        return 300 - goldAmount;
+    }
+
+}
