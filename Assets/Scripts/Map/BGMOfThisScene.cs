beac3eafafce431e1950117026b1edeb799f8ca4
jyll sarta <illstarred.dive@gmail.com>
2018-06-17 22:18:43 +0900

    BGM遷移

---
+++ Assets/Scripts/Map/BGMOfThisScene.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BGMOfThisScene : MonoBehaviour {
+
+    [SerializeField]
+    private AudioSource battle;
+    [SerializeField]
+    private AudioSource boss;
+    [SerializeField]
+    private AudioSource field;
+
+    // Use this for initialization
+    void Start () {
+        field.Play();
+        SceneManager.sceneLoaded += OnSceneLoaded;
+        SceneManager.sceneUnloaded += OnSceneUnLoaded;
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        switch (scene.name)
+        {
+            case "battleAlpha":
+                field.Pause();
+                battle.Play();
+                break;
+        }
+    }
+
+    void OnSceneUnLoaded(Scene scene)
+    {
+        switch (scene.name)
+        {
+            case "battleAlpha":
+                field.UnPause();
+                break;
+
+        }
+    }
+
+}
